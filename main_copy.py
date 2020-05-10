bbef6695d84d5793ecc40f9e980b0c974b838005
robivirt <robivirt@mail.ru>
2020-05-10 20:24:29 +0500

    Окончательное исправление багов

--- main_copy.py
+++ main_copy.py
@@ -95,5 +95,10 @@ def logout():
     return redirect(url_for('index'))
 
 
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1', debug=True)
