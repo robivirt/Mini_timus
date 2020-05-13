4e7343fd2b864eb648c7b57872575f5555d1191f
Артём Степанов <51709851+ArtyomStepanov@users.noreply.github.com>
2020-05-13 13:32:37 +0500

    Update users.py

--- data/users.py
+++ data/users.py
@@ -1,5 +1,6 @@
 import sqlalchemy
 from sqlalchemy import orm
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from .db_session import SqlAlchemyBase_users
 from flask_login import UserMixin
@@ -8,9 +9,15 @@ from flask_login import UserMixin
 class Users(SqlAlchemyBase_users, UserMixin):
     __tablename__ = 'users'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, 
+    id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
     username = sqlalchemy.Column(sqlalchemy.String)
     email = sqlalchemy.Column(sqlalchemy.String)
     password = sqlalchemy.Column(sqlalchemy.String)
     solve_problems = sqlalchemy.Column(sqlalchemy.String, default="")
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
